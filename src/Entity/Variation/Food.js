942e0ae51a76085cdc42e80ba6f9fcca9ee33380
Gerfey <sanjia@live.ru>
2022-12-08 18:00:49 +0700

    первая версия бекенда игры

---
+++ src/Entity/Variation/Food.js
@@ -0,0 +1,58 @@
+const Entity = require('../Entity')
+
+const RadiusEntity = require('../Properties/RadiusEntity')
+const MassEntity = require('../Properties/MassEntity')
+
+const EventBus = require('../../Message/EventBus')
+const EventMessage = require('../../Message/EventMessage')
+
+class Food extends Entity {
+  constructor(x, y, code, color) {
+    super(code, x, y)
+
+    this.type = 'food';
+
+    this.radius = new RadiusEntity(25, 10)
+    this.mass = new MassEntity(0.1, 2, 0.0005, Math.random() * (2 - 0.1) + 0.1)
+    this.color = color
+  }
+
+  update() {
+    this.radius.value = this.mass.value * this.radius.factor;
+  }
+
+  onTriggerEnter(entity) {
+    if (this.mass.value > entity.mass.value) {
+      this.addMass(entity)
+      entity.removeMass(this)
+    }
+
+    if (this.mass.value <= this.mass.min) {
+      EventBus.emit(EventMessage.FOOD.DESTROY, this.id)
+    }
+  }
+
+  addMass(entity) {
+    if (this.mass.value >= this.mass.max) {
+      this.mass.value = this.mass.max
+      return
+    }
+
+    this.mass.value += this.deltaTime * entity.mass.value / 2
+  }
+
+  removeMass(entity) {
+    this.mass.value -= this.deltaTime * entity.mass.value / 2
+  }
+
+  serializeForUpdate() {
+    return {
+      ...(super.serializeForUpdate()),
+      radius: this.radius.value,
+      mass: this.mass.value,
+      color: this.color
+    }
+  }
+}
+
+module.exports = Food
